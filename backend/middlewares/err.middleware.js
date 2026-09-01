2c35e6f58cc67e732a94da0cb99feb6e643a7688
Mai Khalid <maikhalid500@gmail.com>
2026-09-02 00:38:46 +0300

    Update err.middleware.js

--- backend/middlewares/err.middleware.js
+++ backend/middlewares/err.middleware.js
@@ -1,5 +1,4 @@
 module.exports = (err, req, res, next) => {
-  // 1. Mongoose Validation Error
   if (err.name === "ValidationError") {
     return res.status(400).json({
       message: "Validation error",
@@ -7,7 +6,6 @@ module.exports = (err, req, res, next) => {
     });
   }
 
-  // 2. Duplicate Key Error
   if (err.code === 11000) {
     const field = Object.keys(err.keyValue)[0];
 
@@ -16,21 +14,18 @@ module.exports = (err, req, res, next) => {
     });
   }
 
-  // 3. Invalid ObjectId / Cast Error
   if (err.name === "CastError") {
     return res.status(400).json({
       message: `Invalid ${err.path}`,
     });
   }
 
-  // 4. Custom Error
   if (err.status) {
     return res.status(err.status).json({
       message: err.message,
     });
   }
 
-  // 5. Unknown / Server Error
   return res.status(500).json({
     message: err.message || "Internal Server Error",
   });
