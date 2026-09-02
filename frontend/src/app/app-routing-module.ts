f3701a9476b35f3dae7b9cf5c90065f8f7919f41
Moaz Wael <moazwael.dev@gmail.com>
2026-09-03 01:02:51 +0300

    feat: Implement noAuthGuard for route protection

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -5,16 +5,17 @@ import { Login } from './login/login';
 import { Register } from './register/register';
 import { Dashboard } from './dashboard/dashboard';
 import { authGuard } from './guards/auth-guard';
+import { noAuthGuard } from './guards/no-auth-guard';
 
 const routes: Routes = [
-  { path: '', component: Login },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+  { path: '', component: Login, canActivate: [noAuthGuard] },
+  { path: 'login', component: Login, canActivate: [noAuthGuard] },
+  { path: 'register', component: Register, canActivate: [noAuthGuard] },
 
   {
     path: 'dashboard',
     component: Dashboard,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
   },
 ];
 
